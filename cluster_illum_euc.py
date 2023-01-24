4916bd8d8ec3e7631bb639da98f868af1624ffb7
DY112 <dongyoung.kim@yonsei.ac.kr>
2023-01-24 13:12:47 +0900

    add nikon,sony

--- cluster_illum_euc.py
+++ cluster_illum_euc.py
@@ -10,8 +10,9 @@ import numpy as np
 from sklearn.cluster import KMeans
 
 N_clusters = 5
+CAMERA = 'nikon'
 
-with open('meta.json','r') as json_file:
+with open(f'meta_{CAMERA}.json','r') as json_file:
     meta = json.load(json_file)
 
 index = []
@@ -40,7 +41,7 @@ for place in meta:
 # visualize initial illum chroma plot
 illum_chroma = np.array(illum_chroma)
 plt.plot(illum_chroma[:,:1],illum_chroma[:,1:],'ro', alpha=0.5, markersize=3)
-plt.savefig('cluster_result/initinal_plot.png')
+plt.savefig(f'cluster_result/{CAMERA}_initinal_plot.png')
 plt.clf()
 
 # K means clustering & plot
@@ -60,7 +61,7 @@ labels = kmeans.labels_
 plt.scatter(illum_chroma[:,:1],illum_chroma[:,1:],c=labels,s=3**2)
 # plot centerpoints of clusters
 plt.plot(cluster_centers[:,:1],cluster_centers[:,1:], 'rx')
-plt.savefig('cluster_result/cluster_plot.png')
+plt.savefig(f'cluster_result/{CAMERA}_{N_clusters}_cluster_plot.png')
 plt.clf()
 
 # plot distributions of labels
@@ -68,15 +69,15 @@ dist = []
 for i in range(N_clusters):
     dist.append(len(labels[labels==i]))
 plt.plot(range(N_clusters),dist)
-plt.savefig('cluster_result/dist_labels.png')
+plt.savefig(f'cluster_result/{CAMERA}_{N_clusters}_dist_labels.png')
 plt.clf()
 
 # plot center point of clusters
 plt.plot(cluster_centers[:,:1],cluster_centers[:,1:], 'bo')
-plt.savefig('cluster_result/centers_plot.png')
+plt.savefig(f'cluster_result/{CAMERA}_{N_clusters}_centers_plot.png')
 plt.clf()
 # save center coordinates of clusters
-np.save('cluster_result/cluster.npy',cluster_centers)
+np.save(f'cluster_result/{CAMERA}_{N_clusters}_cluster.npy',cluster_centers)
 
 # insert illuminant cluster data to Json & save Json
 for i,idx in enumerate(index):
@@ -84,8 +85,8 @@ for i,idx in enumerate(index):
     cluster_no = labels[i]
     meta[place][light+'_cluster'] = int(cluster_no)
 
-with open('meta_clustered.json', 'w') as out_file:
+with open(f'{CAMERA}_meta_{N_clusters}_clustered.json', 'w') as out_file:
     json.dump(meta, out_file, indent=4)
 
-with open('cluster_result/kmeans', 'wb') as pickle_file:
+with open(f'cluster_result/{CAMERA}_{N_clusters}_kmeans', 'wb') as pickle_file:
     pickle.dump({'kmeans':kmeans}, pickle_file)
